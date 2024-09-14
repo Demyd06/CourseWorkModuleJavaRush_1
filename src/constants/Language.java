4ee73cdbba8ea2127f958f098e5abb554613dd10
Demyd <demidd06@gmail.com>
2024-09-14 17:35:26 +0300

    Update

--- src/constants/Language.java
+++ src/constants/Language.java
@@ -2,13 +2,28 @@ package constants;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Language {
-   public static final ArrayList<Character> english = new ArrayList<>(Arrays.asList(
+   public static final ArrayList<Character> ENGLISH = new ArrayList<>(Arrays.asList(
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
             'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
             'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
             'w', 'x', 'y', 'z'
     ));
+    public static final ArrayList<Character> UKRAINIAN = new ArrayList<>(Arrays.asList(
+            'А', 'Б', 'В', 'Г', 'Ґ', 'Д', 'Е', 'Є', 'Ж', 'З', 'И', 'І',
+            'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
+            'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ю', 'Я', 'а', 'б', 'в', 'г',
+            'ґ', 'д', 'е', 'є', 'ж', 'з', 'и', 'і', 'й', 'к', 'л', 'м',
+            'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш',
+            'щ', 'ь', 'ю', 'я'
+    ));
+    public static final List<String> COMMON_ENGLISH_WORDS = Arrays.asList(
+            "the", "and", "is", "in", "with", "to", "of", "on", "for", "a", "at", "as", "that", "it"
+    );
+    public static final List<String> COMMON_UKRAINIAN_WORDS = Arrays.asList(
+            "і", "та", "це", "в", "на", "з", "до", "у", "як", "що", "не", "так", "але"
+    );
 }
--- src/crypters/Bruteforce.java
+++ src/crypters/Bruteforce.java
@@ -1,18 +1,44 @@
 package crypters;
 
+import constants.CommonWords;
 import constants.Language;
 
 public class Bruteforce {
+    private int BruteForceKey;
+
     public String BruteForceDecrypted(String input) {
         StringBuilder bruteForceBuilder = new StringBuilder();
         Decrypter decrypter = new Decrypter();
-        for (int key = 1; key <= Language.english.size(); key++) {
-            String decrypted = decrypter.Decrypt(input,key);
+        String bestMatch = "";
+        int bestScore = 0;
 
-            if(decrypted.contains("the") || decrypted.contains("with")) {
-                bruteForceBuilder.append(decrypted);
+        for (int key = 1; key <= Language.ENGLISH.size(); key++) {
+            String decrypted = decrypter.Decrypt(input, key);
+            int score = countCommonWords(decrypted);
+
+            // Якщо текст має більше типових слів, запам'ятовуємо його як найкращий варіант
+            if (score > bestScore) {
+                bestScore = score;
+                bestMatch = decrypted;
+                setBruteForceKey(key);
             }
         }
+
+        bruteForceBuilder.append(bestMatch);
         return bruteForceBuilder.toString();
+
     }
+    private int countCommonWords(String text) {
+        int count = 0;
+        String[] words = text.toLowerCase().split("\\s+");
+        for (String word : words) {
+            if (CommonWords.COMMON_ENGLISH_WORDS.contains(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int getBruteForceKey() {return BruteForceKey;}
+    public void setBruteForceKey(int bruteForceKey) {BruteForceKey = bruteForceKey;}
 }
